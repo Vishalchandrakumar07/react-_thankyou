196e647d9cf086119e154f7f5e78bf17d1fc6418
vishalchandrakumar07 <vishalchandrakumar07@gmail.com>
2025-09-03 00:11:48 +0530

    jhv

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Thanks!</h1>
+      <p>We respect your decision.</p>
+    </div>
+  );
+}
+
+export default App;
